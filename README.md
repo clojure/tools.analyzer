abb392c6942ccd8836fa77c781f31469f9fc848a
Alex Miller <alex@puredanger.com>
2013-10-04 11:28:49 -0500

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# tools.analyzer
+
+An analyzer for Clojure code, written in Clojure and producing AST in EDN
+
+## Releases and Dependency Information
+
+
+
+## License
+
+Copyright © 2013 Nicola Mometto, Rich Hickey & contributors.
+
+Distributed under the Eclipse Public License, the same as Clojure.
+
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject tools.analyzer "0.0.1-SNAPSHOT"
+  :description "An analyzer for Clojure code, written in Clojure and producing AST in EDN."
+  :url "https://github.com/clojure/tools.analyzer"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
