877323ed5951ec63db332af6dddb83fa376c9f7a
Bronsa <brobronsa@gmail.com>
2013-11-13 00:42:28 +0100

    add tests for most passes

--- src/test/clojure/clojure/tools/analyzer/passes_test.clj
+++ src/test/clojure/clojure/tools/analyzer/passes_test.clj
@@ -1,7 +1,13 @@
 (ns clojure.tools.analyzer.passes-test
   (:refer-clojure :exclude [macroexpand-1])
   (:require [clojure.tools.analyzer.passes :refer :all]
-            [clojure.test :refer [deftest is]]))
+            [clojure.test :refer [deftest is]]
+            [clojure.set :as set]
+            [clojure.tools.analyzer.core-test :refer [ast e]]
+            [clojure.tools.analyzer.passes.add-binding-atom :refer [add-binding-atom]]
+            [clojure.tools.analyzer.passes.source-info :refer [source-info]]
+            [clojure.tools.analyzer.passes.uniquify :refer [uniquify-locals]]
+            [clojure.tools.analyzer.passes.constant-lifter :refer [constant-lift]]))
 
 (deftest passes-utils-test
   (let [ast {:foo [{:a 1} {:a 2}] :bar [{:a 3}] :children [:foo :bar]}]
@@ -25,3 +31,28 @@
                                ast) :reversed))
                      @a)))
     (is (= [[{:a 1} {:a 2}] [{:a 3}]] (children ast)))))
+
+(deftest add-binding-atom-test
+  (let [the-ast (add-binding-atom (ast (let [a 1] a)))]
+    (swap! (-> the-ast :bindings first :atom) assoc :a 1)
+    (is (= 1 (-> the-ast :body :ret :atom deref :a)))))
+
+(deftest source-info-test
+  (is (= 1 (-> {:form ^{:line 1} [1]} source-info :env :line)))
+  (is (= 1 (-> {:form ^{:column 1} [1]} source-info :env :column))))
+
+(def ^:const pi 3.14)
+
+(deftest constant-lift-test
+  (swap! (:namespaces e) assoc-in ['user :mappings 'pi] #'pi)
+  (is (= :const (-> (ast {:a {:b :c}}) (postwalk constant-lift) :op)))
+  (is (not= :const (-> (ast {:a {:b #()}}) (postwalk constant-lift) :op)))
+  (is (= :const (-> (ast [:foo 1 "bar" #{#"baz" {23 user/pi}}])
+                  (postwalk constant-lift) :op))))
+
+(deftest uniquify-test
+  (let [the-ast (uniquify-locals (ast (let [x 1 y x x (let [x x] x)]
+                                        (fn [y] x))))]
+    (is (= 'x__#2 (-> the-ast :body :ret :methods first :body :ret :name)))
+    (is (= 'y__#1 (-> the-ast :body :ret :methods first :params first :name)))
+    (is (apply not= (->> the-ast :bindings (mapv :name))))))
