a2a6aca312bcf9fe4b27e046c1ff29656a68878d
Bronsa <brobronsa@gmail.com>
2013-11-14 04:59:41 +0100

    add tests for emit-form

--- src/test/clojure/clojure/tools/analyzer/passes_test.clj
+++ src/test/clojure/clojure/tools/analyzer/passes_test.clj
@@ -7,7 +7,8 @@
             [clojure.tools.analyzer.passes.add-binding-atom :refer [add-binding-atom]]
             [clojure.tools.analyzer.passes.source-info :refer [source-info]]
             [clojure.tools.analyzer.passes.uniquify :refer [uniquify-locals]]
-            [clojure.tools.analyzer.passes.constant-lifter :refer [constant-lift]]))
+            [clojure.tools.analyzer.passes.constant-lifter :refer [constant-lift]]
+            [clojure.tools.analyzer.passes.emit-form :refer [emit-form emit-hygienic-form]]))
 
 (deftest passes-utils-test
   (let [ast {:foo [{:a 1} {:a 2}] :bar [{:a 3}] :children [:foo :bar]}]
@@ -56,3 +57,36 @@
     (is (= 'x__#2 (-> the-ast :body :ret :methods first :body :ret :name)))
     (is (= 'y__#1 (-> the-ast :body :ret :methods first :params first :name)))
     (is (apply not= (->> the-ast :bindings (mapv :name))))))
+
+
+(deftest emit-form-test
+  (is (= 1 (emit-form (ast 1))))
+  (is (= "a" (emit-form (ast "a"))))
+  (is (= :foo/bar (emit-form (ast :foo/bar))))
+  (is (= 'a (emit-form (ast a))))
+  (is (= 'a/b (emit-form (ast a/b))))
+  (is (= 'a.b (emit-form (ast a.b))))
+  (is (= 'a.b/c (emit-form (ast a.b/c))))
+  (is (= '(. b (a c))) (emit-form (ast (.a b c))))
+  (is (= '(. b (a c))) (emit-form (ast (.a b (c)))))
+  (is (= '(.-a b) (emit-form (ast (.-a b)))))
+  (is (= '(. b a) (emit-form (ast (.a b)))))
+  (is (= '(let* [a 1] a) (emit-form (ast (let [a 1] a)))))
+  (is (= '(fn* ([] nil)) (emit-form (ast (fn [])))))
+  (is (= '(fn* ([] nil) ([a] nil)) (emit-form (ast (fn ([]) ([a]))))))
+  (is (= '(loop* [a 1] (recur 2)) (emit-form (ast (loop [a 1] (recur 2))))))
+  (is (= ''a (emit-form (ast 'a))))
+  (is (= [1 2 3] (emit-form (ast [1 2 3]))))
+  (is (= {:a 1 [:b] 2} (emit-form (ast {:a 1 [:b] 2}))))
+  (is (= {:a 1} (meta (emit-form (ast ^{:a 1} [:foo])))))
+  (is (= 1 (emit-form (ast (do 1)))))
+  (is (= '(do a b c) (emit-form (ast (do a b c)))))
+  (is (= '(if 1 2) (emit-form (ast (if 1 2)))))
+  (is (= '(if 1 2 3) (emit-form (ast (if 1 2 3)))))
+  (is (= '(new a b c) (emit-form (ast (a. b c)))))
+  (is (= '(set! a 1) (emit-form (ast (set! a 1)))))
+  (is (= '(def a 1) (emit-form (ast (def a 1)))))
+  (is (= '(def a "doc" 1) (emit-form (ast (def a "doc" 1)))))
+  (is (= '(a b) (emit-form (ast (a b)))))
+  (is (= '(try (throw 1) (catch e t b) (finally 2))
+         (emit-form (ast (try (throw 1) (catch e t b) (finally 2)))))))
