b51528e6fab5512a9f8f21802dcb480447025828
Tom Faulhaber <git_net@infolace.com>
2014-03-01 18:45:30 -0800

    Autodoc commit for master/19b9d42b

--- index-0.1.0.clj
+++ index-0.1.0.clj
@@ -67,7 +67,7 @@
    :name "clojure.tools.analyzer.passes.trim",
    :doc nil}
   {:source-url
-   "https://github.com/clojure/tools.analyzer/blob/81e260eccfb95053c1c75231159591c6de86d226/src/main/clojure/clojure/tools/analyzer/passes/uniquify.clj",
+   "https://github.com/clojure/tools.analyzer/blob/19b9d42b575c63a5a19878f45772210c6a125068/src/main/clojure/clojure/tools/analyzer/passes/uniquify.clj",
    :wiki-url
    "http://clojure.github.com/tools.analyzer/clojure.tools.analyzer.passes.uniquify-api.html",
    :name "clojure.tools.analyzer.passes.uniquify",
@@ -595,13 +595,13 @@
    :name "uniquify-locals",
    :namespace "clojure.tools.analyzer.passes.uniquify",
    :source-url
-   "https://github.com/clojure/tools.analyzer/blob/81e260eccfb95053c1c75231159591c6de86d226/src/main/clojure/clojure/tools/analyzer/passes/uniquify.clj#L77",
+   "https://github.com/clojure/tools.analyzer/blob/19b9d42b575c63a5a19878f45772210c6a125068/src/main/clojure/clojure/tools/analyzer/passes/uniquify.clj#L77",
    :raw-source-url
-   "https://github.com/clojure/tools.analyzer/raw/81e260eccfb95053c1c75231159591c6de86d226/src/main/clojure/clojure/tools/analyzer/passes/uniquify.clj",
+   "https://github.com/clojure/tools.analyzer/raw/19b9d42b575c63a5a19878f45772210c6a125068/src/main/clojure/clojure/tools/analyzer/passes/uniquify.clj",
    :wiki-url
    "http://clojure.github.com/tools.analyzer//clojure.tools.analyzer-api.html#clojure.tools.analyzer.passes.uniquify/uniquify-locals",
    :doc
-   "Walks the AST performing alpha-conversion on local\nbindings' :name field",
+   "Walks the AST performing alpha-conversion on the :name field\nof :local/:binding nodes, invalidates :local map in :env field",
    :var-type "function",
    :line 77,
    :file "src/main/clojure/clojure/tools/analyzer/passes/uniquify.clj"}
