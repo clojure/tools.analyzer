e9b69bcf3e4697a5560890ebc1fa8b949ed0e63e
Nicola Mometto <brobronsa@gmail.com>
2014-09-22 22:02:29 +0200

    provide and use slightly more efficient versions of select-keys and merge, using transients

--- src/main/clojure/clojure/tools/analyzer/utils.clj
+++ src/main/clojure/clojure/tools/analyzer/utils.clj
@@ -177,15 +177,32 @@
                    (>= argc (- (count last-arglist) 2)))
           last-arglist))))
 
+(defn select-keys'
+  "Like clojure.core/select-keys, but uses transients and doesn't preserve meta"
+  [map keyseq]
+  (loop [ret (transient {}) keys (seq keyseq)]
+    (if keys
+      (let [entry (find map (first keys))]
+        (recur (if entry
+                 (conj! ret entry)
+                 ret)
+               (next keys)))
+      (persistent! ret))))
+
+(defn merge'
+  "Like merge, but uses transients"
+  [& mms]
+  (reduce into {} mms))
+
 (defn source-info
   "Returns the available source-info keys from a map"
   [m]
-  (select-keys m #{:file :line :column :end-line :end-column :source-span}))
+  (select-keys' m #{:file :line :column :end-line :end-column :source-span}))
 
 (defn -source-info
   "Returns the source-info of x"
   [x env]
-  (merge
+  (merge'
    (source-info env)
    (when-let [file (and (not= *file* "NO_SOURCE_FILE")
                         *file*)]
@@ -207,4 +224,4 @@
 
 (def mmerge
   "Same as (fn [m1 m2] (merge-with merge m2 m1))"
-  #(merge-with merge %2 %1))
+  #(merge-with merge' %2 %1))
