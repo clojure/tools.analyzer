clojure/tools.analyzer

commit d6dca05f145aa21f3b9e1ea0d3c8be9f40ae3d8b

reduce the number of times meta is called on vars

src/main/clojure/clojure/tools/analyzer/passes/warn_earmuff.clj
@@ -19,7 +19,7 @@
                (> (count name) 2)  ;; Allow * and ** as non-dynamic names
                (.startsWith name "*")
                (.endsWith name "*")
-               (not (dynamic? (:var ast))))
+               (not (dynamic? (:var ast) (:meta ast))))
       (binding [*out* *err*]
         (println "Warning:" name "not declared dynamic and thus is not dynamically rebindable,"
                  "but its name suggests otherwise."

src/main/clojure/clojure/tools/analyzer.clj
@@ -252,10 +252,12 @@
                        :children    (vec (remove #{:init} children))})
                (if-let [var (let [v (resolve-var sym env)]
                               (and (var? v) v))]
-                 {:op          :var
-                  :assignable? (dynamic? var) ;; we cannot statically determine if a Var is in a thread-local context
-                                              ;; so checking whether it's dynamic or not is the most we can do
-                  :var         var}
+                 (let [m (meta var)]
+                   {:op          :var
+                    :assignable? (dynamic? var m) ;; we cannot statically determine if a Var is in a thread-local context
+                                                  ;; so checking whether it's dynamic or not is the most we can do
+                    :var         var
+                    :meta        m})
                  (if-let [maybe-class (namespace sym)] ;; e.g. js/foo.bar or Long/MAX_VALUE
                    (let [maybe-class (symbol maybe-class)]
                      {:op    :maybe-host-form

src/main/clojure/clojure/tools/analyzer/utils.clj
@@ -122,30 +122,35 @@
 
 (defn private?
   "Returns true if the var is private"
-  [var]
-  (:private (meta var)))
+  ([var] (private? var nil))
+  ([var m]
+     (:private (or m (meta var)))))
 
 (defn macro?
   "Returns true if the var maps to a macro"
-  [var]
-  (:macro (meta var)))
+  ([var] (macro? var nil))
+  ([var m]
+     (:macro (or m (meta var)))))
 
 (defn constant?
   "Returns true if the var is a const"
-  [var]
-  (:const (meta var)))
+  ([var] (constant? var nil))
+  ([var m]
+     (:const (or m (meta var)))))
 
 (defn dynamic?
   "Returns true if the var is dynamic"
-  [var]
-  (or (:dynamic (meta var))
-      (when (var? var) ;; workaround needed since Clojure doesn't always propagate :dynamic
-        (.isDynamic ^Var var))))
+  ([var] (dynamic? var nil))
+  ([var m]
+     (or (:dynamic (or m (meta var)))
+         (when (var? var) ;; workaround needed since Clojure doesn't always propagate :dynamic
+           (.isDynamic ^Var var)))))
 
 (defn protocol-node?
   "Returns true if the var maps to a protocol function"
-  [var]
-  (boolean (:protocol (meta var)))) ;; conveniently this is true in both clojure and clojurescript
+  ([var] (protocol-node? var nil))
+  ([var m]
+     (boolean (:protocol (or m (meta var)))))) ;; conveniently this is true in both clojure and clojurescript
 
 (defn resolve-ns
   "Resolves the ns mapped by the given sym in the env"

commit 3af99e2a8055c22609de2d64b1e0009e949c252d

fix regression introduced with d6dca05f145aa21f3b9e1ea0d3c8be9f40ae3d8b

src/main/clojure/clojure/tools/analyzer/passes/warn_earmuff.clj
@@ -19,7 +19,7 @@
                (> (count name) 2)  ;; Allow * and ** as non-dynamic names
                (.startsWith name "*")
                (.endsWith name "*")
-               (not (dynamic? (:var ast) (:meta ast))))
+               (not (dynamic? (:var ast) (:val (:meta ast)))))
       (binding [*out* *err*]
         (println "Warning:" name "not declared dynamic and thus is not dynamically rebindable,"
                  "but its name suggests otherwise."