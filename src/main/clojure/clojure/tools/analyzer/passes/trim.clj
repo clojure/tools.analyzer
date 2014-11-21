988d8db10190b358ad3805c625197b1a65d5a339
Nicola Mometto <brobronsa@gmail.com>
2014-11-21 21:13:43 +0100

    re-add trim pass

---
+++ src/main/clojure/clojure/tools/analyzer/passes/trim.clj
@@ -0,0 +1,32 @@
+;; Copyright (c) Nicola Mometto, Rich Hickey & contributors.
+;; The use and distribution terms for this software are covered by the
+;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;; which can be found in the file epl-v10.html at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+
+(ns clojure.tools.analyzer.passes.trim
+  (:require [clojure.tools.analyzer.passes.elide-meta :refer [elide-meta]]))
+
+(defmulti trim
+  "Trims the AST of unnecessary nodes, e.g. (do (do 1)) -> 1"
+  {:pass-info {:walk :none :depends #{} :after #{#'elide-meta}}}
+  :op)
+
+(defmethod trim :default [ast] ast)
+
+(defmethod trim :do
+  [{:keys [statements ret] :as ast}]
+  (if (every? :literal? statements)
+    (into (dissoc ast :children :statements :ret) ret)
+    ast))
+
+;; TODO: letfn/loop
+(defmethod trim :let
+  [{:keys [bindings body] :as ast}]
+  (if (or (and (every? (comp :literal? :init) bindings)
+               (:literal? body))
+          (empty? bindings))
+    (into (dissoc ast :children :bindings :body) body)
+    ast))
