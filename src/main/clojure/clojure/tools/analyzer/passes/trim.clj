992e3171648d5bf791d0b19c0c0994642ae20044
Nicola Mometto <brobronsa@gmail.com>
2014-12-04 16:41:19 +0100

    make trim walk the ast

--- src/main/clojure/clojure/tools/analyzer/passes/trim.clj
+++ src/main/clojure/clojure/tools/analyzer/passes/trim.clj
@@ -7,27 +7,25 @@
 ;; You must not remove this notice, or any other, from this software.
 
 (ns clojure.tools.analyzer.passes.trim
-  (:require [clojure.tools.analyzer.passes.elide-meta :refer [elide-meta]]))
+  (:require [clojure.tools.analyzer.passes.elide-meta :refer [elide-meta]]
+            [clojure.tools.analyzer.ast :refer [postwalk]]))
 
-(defmulti trim
-  "Trims the AST of unnecessary nodes, e.g. (do (do 1)) -> 1"
-  {:pass-info {:walk :none :depends #{} :after #{#'elide-meta}}}
-  :op)
+(defmulti -trim :op)
 
-(defmethod trim :default [ast] ast)
+(defmethod -trim :default [ast] ast)
 
 (defn preserving-raw-forms [{:keys [form raw-forms] :as ast} body]
   (let [raw-forms (reverse (cons form raw-forms))]
     (update-in (into ast body) [:raw-forms] into raw-forms)))
 
-(defmethod trim :do
+(defmethod -trim :do
   [{:keys [statements ret] :as ast}]
   (if (every? :literal? statements)
     (preserving-raw-forms (dissoc ast :children :statements :ret) ret)
     ast))
 
 ;; TODO: letfn/loop
-(defmethod trim :let
+(defmethod -trim :let
   [{:keys [bindings body] :as ast}]
   (if (or (and (every? (comp :literal? :init) bindings)
                (:literal? body))
@@ -35,9 +33,15 @@
     (preserving-raw-forms (dissoc ast :children :bindings :body) body)
     ast))
 
-(defmethod trim :try
+(defmethod -trim :try
   [{:keys [catches finally body] :as ast}]
   (if (and (empty? catches)
            (empty? finally))
     (preserving-raw-forms (dissoc ast :children :body :finally :catches) body)
     ast))
+
+(defn trim
+  "Trims the AST of unnecessary nodes, e.g. (do (do 1)) -> 1"
+  {:pass-info {:walk :none :depends #{} :after #{#'elide-meta}}}
+  [ast]
+  (postwalk ast -trim))
