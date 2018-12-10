3a730e09e15d7c6b9c8540eb1740ac41393a8039
Nicola Mometto <me@bronsa.net>
2018-12-10 13:57:25 +0000

    update project.clj

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojure/tools.analyzer "0.6.8-SNAPSHOT"
+(defproject org.clojure/tools.analyzer "0.6.10-SNAPSHOT"
   :description "An analyzer for Clojure code, written in Clojure and producing AST in EDN."
   :url "https://github.com/clojure/tools.analyzer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/main/clojure"]
   :test-paths ["src/test/clojure"]
-  :dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.datomic/datomic-free "0.9.5327" :scope "provided" :exclusions [joda-time]]]
-  :repositories [["sonatype" "http://oss.sonatype.org/content/repositories/releases"]
-                 ["snapshots" "http://oss.sonatype.org/content/repositories/snapshots"]])
+  :repositories [["sonatype" "https://oss.sonatype.org/content/repositories/releases"]
+                 ["snapshots" "https://oss.sonatype.org/content/repositories/snapshots"]])
