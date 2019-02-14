73e3a271afb15504936928385fee6a8cb6c7f5aa
Nicola Mometto <me@bronsa.net>
2019-02-14 17:03:03 +0000

    update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure/tools.analyzer "0.6.10-SNAPSHOT"
+(defproject org.clojure/tools.analyzer "0.7.1-SNAPSHOT"
   :description "An analyzer for Clojure code, written in Clojure and producing AST in EDN."
   :url "https://github.com/clojure/tools.analyzer"
   :license {:name "Eclipse Public License"
